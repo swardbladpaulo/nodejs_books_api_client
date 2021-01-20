b27a6d8a3d768ddc98055ebc5228688cc29556e2
swardbladpaulo <paulo@swardblads.se>
2021-01-20 15:30:33 +0100

    modify app.js and index.html

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const displayBooks = async () => {
   const displayNode = document.getElementById('display')
   const data = await (await fetch(apiUrl + '/books')).json()
   data.books.forEach(book => {
+    debugger
     const displayBookNode = document.createElement('div')
     let html = `<p>${book.title} by: ${book.author}</p>`
     displayBookNode.innerHTML = html
